58c728fbae68290343e5b4b1f2dfa9a403e94912
EvansVan <evansndiritu@gmail.com>
2021-04-04 10:54:33 +0300

    add .gitignore

--- ussme/settings.py
+++ ussme/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ STATIC_ROOT = 'os.path.join(BASE_DIR, ‘static’)'
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '44+2d$0&w&53g$&3-=t&il2wot==0f00876f9(lp0vbp(-n=t8'
+# this is to replace the secret key you cut away before
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
